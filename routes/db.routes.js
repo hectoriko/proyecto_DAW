17964904f0ecfc0cee8c68a3eea4d1e5bfb8f73a
agarrigu <agarrigu@protonmail.com>
2023-04-14 21:02:29 +0200

    wip

--- routes/db.routes.js
+++ routes/db.routes.js
@@ -49,23 +49,6 @@ exports.signup = (req, res) => {
     password_hash: bcrypt.hashSync(req.body.password, 8),
   });
 
-  user.save((err, user) => {
-    if (err) {
-      res.status(500).send({ message: err });
-      return;
-    } else {
-        user.save((err) => {
-          if (err) {
-            res.status(500).send({ message: err });
-            return;
-          }
-          res.send({ message: "User was registered successfully!" });
-        });
-      });
-    }
-  });
-};
-
 // exports.signin = (req, res) => {
 //   User.findOne({
 //     username: req.body.username,
@@ -75,24 +58,19 @@ exports.signup = (req, res) => {
 //         res.status(500).send({ message: err });
 //         return;
 //       }
-
 //       if (!user) {
 //         return res.status(404).send({ message: "User Not found." });
 //       }
-
 //       var passwordIsValid = bcrypt.compareSync(
 //         req.body.password,
 //         user.password
 //       );
-
 //       if (!passwordIsValid) {
 //         return res.status(401).send({ message: "Invalid Password!" });
 //       }
-
 //       var token = jwt.sign({ id: user.id }, config.secret, {
 //         expiresIn: 86400, // 24 hours
 //       });
-
 //       req.session.token = token;
 
 //       res.status(200).send({
